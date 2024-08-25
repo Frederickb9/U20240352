d611b6cc462170f5e2226d9259b378308812ee77
Frederick999 <frederickbenavidez52@gmail.com>
2024-08-24 18:03:45 -0600

    .

--- Tarea Fecha y Hora/ejercicio3.js
+++ Tarea Fecha y Hora/ejercicio3.js
@@ -1,5 +1,3 @@
-//Escribe un programa que obtenga el nombre del mes actual y lo muestre en la consola
-
 let fecha = new Date()
 let mes = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
 console.log(mes[fecha.getMonth()])
---
+++ Tarea Fecha y Hora/ejercicio4.js
@@ -0,0 +1,12 @@
+function diferenciaDias(fecha1, fecha2){
+    let fechaInicial = new Date(fecha1)
+    let fechaFinal = new Date(fecha2)
+    let difTiempo = fechaFinal - fechaInicial
+    const diferenciaDias = difTiempo / (1000 * 60 * 60 * 24)
+    return(diferenciaDias)
+}
+let fecha1 = '1971-06-26'
+let fecha2 = '1977-03-20'
+
+let diasDiferencia = diferenciaDias(fecha1, fecha2)
+console.log(`La diferencia de días entre las dos fechas es: ${diasDiferencia} días`) 
